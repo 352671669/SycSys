cb535af41d44abbe4f390ad3d4b0e37dc8643af7
352671669 <44364444+352671669@users.noreply.github.com>
2020-05-30 14:37:59 +0800

    5/30 14:37

--- syc_application/admin.py
+++ syc_application/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from syc_application.models import Script, ExcuteInfo
 
 # Register your models here.
+
+
+admin.site.register(Script)
+admin.site.register(ExcuteInfo)
